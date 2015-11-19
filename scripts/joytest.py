71012a0f20671b667cf9b57ba797d3337c075c8c
jinah adam <jinahadam@me.com>
2015-11-19 18:46:08 +0800

    send value and control value at 1 hertz

--- scripts/joytest.py
+++ scripts/joytest.py
@@ -4,18 +4,27 @@ import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import Joy
 
+flip = 0
 def talker():
     pub = rospy.Publisher('joytester', Joy, queue_size=10)
     rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(1) # 10hz
     while not rospy.is_shutdown():
         msg = Joy()
-        msg.axes = (1,1,1,1,1,1,1)
+	global flip
+        if (flip == 0):
+        	msg.axes = (0.02,0,0,0,0,0,0)
+                flip = 1
+        else:
+                flip = 0
+        	msg.axes = (0,0,0,0,0,0,0)
         msg.buttons = (0,0,0,0,0,0,0,0,0,0,0)
         pub.publish(msg)
         rate.sleep()
+     
 
 if __name__ == '__main__':
+    
     try:
         talker()
     except rospy.ROSInterruptException:
