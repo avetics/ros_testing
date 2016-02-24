f0caba218a1367f110e54c2c6dfad00f22dc59cc
jinah adam <jinahadam@me.com>
2016-02-24 11:49:30 +0800

    updated

--- scripts/joytest.py
+++ scripts/joytest.py
@@ -6,9 +6,11 @@ from std_msgs.msg import String
 from sensor_msgs.msg import Joy
 from mavros_msgs.msg import RCIn
 from geometry_msgs.msg import Quaternion
+from geometry_msgs.msg import Pose2D
 
-pub = rospy.Publisher('joytester', Joy, queue_size=50)
+pub = rospy.Publisher('joy', Joy, queue_size=50)
 testing_pub = rospy.Publisher('testing_diff', Quaternion, queue_size = 50)
+latency_ch = rospy.Publisher('latency_ch', Pose2D, queue_size = 50)
 
 csv_data = []
 k = 0
@@ -27,7 +29,71 @@ def convert_ppm(ppm_val):
     return result
 
 def callback(data):
+    diff(data)
+    #compare_all(data)
+    #comapre_single_channel(data)
+    
+    
+def listener():
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber("/mavros/rc/in", RCIn, callback)
+    rospy.spin()
 
+def load_data():
+    print("Load Data")
+    global csv_data
+    with open('/home/jinahadam/catkin_ws/src/joy_logger/joy_data.csv', 'rb') as f:
+    	   reader = csv.reader(f)
+           csv_data = list(reader)
+
+
+def comapre_single_channel(data):
+    global k 
+    
+    last_row = csv_data[k-frequency_offset]
+
+    lat = Pose2D()
+    lat.x = float(last_row[1])
+    lat.y = convert_ppm(data.channels[3])
+    lat.theta = 0
+    
+    latency_ch.publish(lat)
+
+    if (k < len(csv_data)):
+        row = csv_data[k]
+        msg = Joy()
+        msg.axes = (float(row[1]),float(row[2]),float(row[3]),float(row[4]),float(row[5]),float(row[6]),float(row[7]))
+        msg.buttons = (0,0,0,0,0,0,0,0,0,0,0)
+        k = k + 1
+        pub.publish(msg)       
+    else:
+        k = 0
+
+def diff(data):
+    global k 
+    
+    last_row = csv_data[k-frequency_offset]
+
+    diff = Quaternion()
+    diff.x = round(float(last_row[1])-convert_ppm(data.channels[3]),4)
+    diff.y = round(float(last_row[2])-convert_ppm(data.channels[1]),4)
+    diff.z = round(float(last_row[4])-convert_ppm(data.channels[0]),4)
+    diff.w = round(float(last_row[5])+convert_ppm(data.channels[2]),4)
+    
+    testing_pub.publish(diff)
+
+    if (k < len(csv_data)):
+        row = csv_data[k]
+        msg = Joy()
+     	msg.axes = (float(row[1]),float(row[2]),float(row[3]),float(row[4]),float(row[5]),float(row[6]),float(row[7]))
+        msg.buttons = (0,0,0,0,0,0,0,0,0,0,0)
+	k = k + 1
+        pub.publish(msg)       
+    else:
+        k = 0
+        
+
+def compare_all(data):
     global k 
     
     last_row = csv_data[k-frequency_offset]
@@ -60,18 +126,6 @@ def callback(data):
         #k = 0
         print("Joystick & RC In data logged with GPS Time")
         exit()
-    
-def listener():
-    rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber("/mavros/rc/in", RCIn, callback)
-    rospy.spin()
-
-def load_data():
-    print("Load Data")
-    global csv_data
-    with open('/home/jinahadam/catkin_ws/src/joy_logger/joy.csv', 'rb') as f:
-    	   reader = csv.reader(f)
-           csv_data = list(reader)
 
 if __name__ == '__main__':
     
